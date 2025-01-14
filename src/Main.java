e9ba831e0d72a3652b5c5685e484a2be379896a5
gshaver <geneshaver@gmail.com>
2025-01-14 14:04:25 -0600

    cs50 mario in java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,27 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Hello and welcome!");
+        for (int i = 1; i <= 2; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        int max = 8;
+        for (int i = 1; i <= max; i++) {
+            System.out.println();
+            for (int j = 1; j <= max - i; j++) {//start
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            System.out.print("  "); //middle
+            for (int j = 1; j <= i; j++) {//end
+                System.out.print("#");
+            }
+        }
+
+
     }
 }
